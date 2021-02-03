b68950258f1b75564accecf761855a8966292c4a
maks-bubela <ya.bubela@gmail.com>
2021-02-03 15:01:42 +0300

    Finish learning Pattern Chain of Responsibility

---
+++ Pattern Chain of Responsibility/Pattern Chain of Responsibility/ATM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern_Chain_of_Responsibility
+{
+    class ATM
+    {
+        public ATM next;
+        public int denomination;
+        public int quantity;
+        private string result = "";
+        private string error = "The ATM is unable to process this request.";
+        public void SetMoney(int denomination, int quantity)
+        {
+            this.denomination = denomination;
+            this.quantity = quantity;
+        }
+        public string hundler(int summ)
+        {
+            string check;
+            for (int i = 0; i < quantity && summ - denomination >= 0; i++)
+            {
+                summ -= denomination;
+                if (summ - denomination <= 0 || i + 1 == quantity)
+                    result += (i + 1) + " : " + denomination + "\n";
+            }
+            if (summ != 0)
+            {
+                if (next != null)
+                {
+                    check = next.hundler(summ);
+                    if (check != error)
+                        result += check;
+                    else
+                        return error;
+                }
+                else
+                    return error;
+            }
+            return result;
+        }
+    }
+}
---
+++ Pattern Chain of Responsibility/Pattern Chain of Responsibility/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pattern_Chain_of_Responsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM atm = new ATM();
+            atm.SetMoney(100, 5);
+
+            atm.next = new ATM();
+            atm.next.SetMoney(50, 10);
+
+            atm.next.next = new ATM();
+            atm.next.next.SetMoney(25, 10);
+
+            atm.next.next.next = new ATM();
+            atm.next.next.next.SetMoney(10, 10);
+
+            atm.next.next.next.next = new ATM();
+            atm.next.next.next.next.SetMoney(5, 30);
+
+            atm.next.next.next.next.next = new ATM();
+            atm.next.next.next.next.next.SetMoney(1, 50);
+
+            Console.WriteLine("Enter summ you want : ");
+            Console.WriteLine( atm.hundler(Convert.ToInt32(Console.ReadLine())));
+            
+        }
+    }
+}
